84ddcc5a693ef25994ba0dccd578fa646b3065e0
Oleh Biriukov <ol.biriukov@gmail.com>
2022-07-15 21:14:21 +0300

    fix

--- src/mgos_arduino_stepper.cpp
+++ src/mgos_arduino_stepper.cpp
@@ -1,8 +1,17 @@
 #include "mgos_arduino_stepper.h"
 
-Stepper *mgos_Stepper_create(int number_of_steps, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4) {
+Stepper *mgos_Stepper_create_2_pins(int number_of_steps, int motor_pin_1, int motor_pin_2) {
+    return new Stepper(number_of_steps, motor_pin_1, motor_pin_2);
+}
+
+Stepper *mgos_Stepper_create_4_pins(int number_of_steps, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4) {
     return new Stepper(number_of_steps, motor_pin_1, motor_pin_2, motor_pin_3, motor_pin_4);
 }
+
+Stepper *mgos_Stepper_create_5_pins(int number_of_steps, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4, int motor_pin_5) {
+    return new Stepper(number_of_steps, motor_pin_1, motor_pin_2, motor_pin_3, motor_pin_4, motor_pin_5);
+}
+
 
 void mgos_Stepper_setSpeed(Stepper *stepper, int speed) {
     if (stepper == nullptr) return;
